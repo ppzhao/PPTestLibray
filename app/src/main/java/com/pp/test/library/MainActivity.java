e43794d1267c2e280796c21087ae410b2224ce13
ppzhao <574520597@qq.com>
2017-10-11 16:14:38 +0800

    修改配置

--- app/src/main/java/com/pp/test/library/MainActivity.java
+++ app/src/main/java/com/pp/test/library/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
-                Toast.makeText(MainActivity.this, TestUtil.getLibraryVersion(),Toast.LENGTH_SHORT);
+                Toast.makeText(MainActivity.this, TestUtil.getLibraryVersion(),Toast.LENGTH_SHORT).show();
             }
         });
     }
