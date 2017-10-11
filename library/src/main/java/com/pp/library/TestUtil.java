510655cb9111d529091db501fb4ae6f6b952dd85
ppzhao <574520597@qq.com>
2017-10-11 15:18:03 +0800

    测试

---
+++ library/src/main/java/com/pp/library/TestUtil.java
@@ -0,0 +1,12 @@
+package com.pp.library;
+
+/**
+ * Created by ZPP on 2017/10/11.
+ */
+
+public class TestUtil {
+
+    public static String getLibraryVersion(){
+        return BuildConfig.VERSION_NAME;
+    }
+}
